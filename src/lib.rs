b8c05c801c30f7ae823a9a9a9f3d1e76f05ec035
Bugsyth <bugsyth@hotmail.com>
2025-02-04 11:14:45 -0500

    added repository and fixed borrowing issues

--- src/lib.rs
+++ src/lib.rs
@@ -14,11 +14,14 @@ use imgui_winit_support::HiDpiMode;
 pub use {
     imgui::{Condition, Context},
     imgui_glium_renderer::Renderer,
+    imgui_glium_renderer::RendererError,
     imgui_winit_support::WinitPlatform,
+    winit::error::ExternalError,
 };
 
 pub const FONT_SIZE: f32 = 13.0;
 
+/// Holds imgui-rs context and winit backend platform state
 pub struct ImGui {
     pub context: Context,
     pub renderer: Renderer,
@@ -26,30 +29,41 @@ pub struct ImGui {
 }
 
 impl ImGui {
-    pub fn get_ui(&mut self, window: &Window) -> &mut Ui {
-        self.platform
-            .prepare_frame(self.context.io_mut(), window)
-            .expect("Failed to prepare ImGui frame");
+    /// Run before getting the ui
+    pub fn prepare_ui(&mut self, window: &Window) -> Result<(), ExternalError> {
+        self.platform.prepare_frame(self.context.io_mut(), window)
+    }
+    pub fn get_ui(&mut self) -> &mut Ui {
         self.context.frame()
     }
-    pub fn render(&mut self, ui: &Ui, renderer: &mut impl Surface, window: &Window) {
+    /// Run before rendering
+    pub fn prepare_render(&mut self, ui: &Ui, window: &Window) {
         self.platform.prepare_render(ui, window);
-        let draw_data = self.context.render();
-        self.renderer
-            .render(renderer, draw_data)
-            .expect("Failed to render ImGui");
     }
+    pub fn render(&mut self, renderer: &mut impl Surface) -> Result<(), RendererError> {
+        let draw_data = self.context.render();
+        self.renderer.render(renderer, draw_data)
+    }
+    /// Pass the engine's delta time into this function
     pub fn update_dt(&mut self, dt: f32) {
         self.context
             .io_mut()
             .update_delta_time(Duration::from_secs_f32(dt));
     }
+    /// Handles a window event.
+    ///
+    /// This function performs the following actions (depends on the event):
+    ///
+    /// - window size / dpi factor changes are applied
+    /// - keyboard state is updated
+    /// - mouse state is updated
     pub fn event(&mut self, window: &Window, event: &WindowEvent) {
         self.platform
             .handle_window_event(self.context.io_mut(), window, event);
     }
 }
 
+/// Builds a context and winit backend
 pub fn init<FInit>(window: &Window, display: &Display<WindowSurface>, mut startup: FInit) -> ImGui
 where
     FInit: FnMut(&mut Context, &mut Renderer, &Display<WindowSurface>) + 'static,
